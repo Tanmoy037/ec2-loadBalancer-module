7a6197022cf0e3ee17c1f4ff3fe710e0a082660e
Tanmoy Santra <108757431+Tanmoy037@users.noreply.github.com>
2023-11-24 13:57:07 +0530

    Update output.tf

--- modules/LoadBalancer/output.tf
+++ modules/LoadBalancer/output.tf
@@ -1,11 +1,7 @@
-output "lb_id" {
-  value = aws_lb.example.id 
-}
-
-output "lb_arn" {
-  value = aws_lb.example.arn
-}
-
 output "lb_dns_name" {
   value = aws_lb.example.dns_name
+}
+
+output "certificate_arn" {
+  value = aws_acm_certificate.example.arn
 }
