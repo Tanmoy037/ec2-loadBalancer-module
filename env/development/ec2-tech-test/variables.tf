6c1d2283e8af8a63a726131d9f31fa5ac8c11fbd
Tanmoy Santra <108757431+Tanmoy037@users.noreply.github.com>
2023-11-24 13:56:43 +0530

    Update variables.tf

--- env/development/ec2-tech-test/variables.tf
+++ env/development/ec2-tech-test/variables.tf
@@ -12,3 +12,45 @@ variable "region" {
     type = string
   
 }
+
+
+variable "protocol" {
+    type = string
+  
+}
+
+variable "port" {
+    type = string
+}
+
+variable "health_check_timeout" {
+    type = string
+  
+}
+
+variable "health_check_interval" {
+    type = string
+  
+}
+
+variable "healthy_threshold" {
+    type = string
+  
+}
+
+variable "unhealthy_threshold" {
+    type = string
+}
+
+variable "domain_name" {
+    type = string
+  
+}
+variable "lb_name" {
+    type = string
+  
+}
+variable "target_group_name" {
+    type = string
+  
+}
