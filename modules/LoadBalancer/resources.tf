f7cbd17a29c5d31dd90a7253eef16fe1e4330ddc
Tanmoy Santra <108757431+Tanmoy037@users.noreply.github.com>
2023-11-24 13:57:31 +0530

    Update resources.tf

--- modules/LoadBalancer/resources.tf
+++ modules/LoadBalancer/resources.tf
@@ -1,22 +1,43 @@
 resource "aws_lb" "example" {
-  name               = "example-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = var.subnet_ids
-
-  enable_deletion_protection = true
+  name            = var.lb_name
+  internal        = var.internal
+  security_groups = [var.security_group_id]
+  subnets         = var.subnet_ids
 }
 
-resource "aws_lb_target_group" "test" {
-  name     = "tf-example-lb-tg"
-  port     = 80
-  protocol = "HTTP"
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.example.arn
+  protocol          = var.protocol
+  port              = var.port
+  certificate_arn = aws_acm_certificate.example.arn
+
+  default_action {
+    target_group_arn = aws_lb_target_group.example.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_target_group" "example" {
+  name     = var.target_group_name
+  port     = var.port
+  protocol = var.protocol
   vpc_id   = var.vpc_id
+
+  health_check {
+    path                = var.health_check_path
+    timeout             = var.health_check_timeout
+    interval            = var.health_check_interval
+    healthy_threshold   = var.healthy_threshold
+    unhealthy_threshold = var.unhealthy_threshold
+  }
 }
 
-resource "aws_lb_target_group_attachment" "test" {
-  target_group_arn = aws_lb_target_group.test.arn
-  target_id        = var.instance_id
-  port             = 80
+resource "aws_acm_certificate" "example" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+}
+
+resource "aws_lb_listener_certificate" "example" {
+  listener_arn     = aws_lb_listener.example.arn
+  certificate_arn  = aws_acm_certificate.example.arn
 }
